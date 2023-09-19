836852ccb408dd914db6d81ab949a686ceac0dbe
Martin Mroz <mamr@hotmail.no>
2023-09-19 13:08:35 +0200

    dims input

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -7,7 +7,6 @@ function CustomGrid() {
       <div className="bg-gray-200 w-full px-5 py-10 grid grid-cols-3 gap-5">
         {[...Array(9)].map((_, idx) => (
           <div key={idx} className="flex flex-col">
-
             {idx === 0 ? (
               <>
                 <label htmlFor={`select-${idx}`} className="mb-2 text-gray-600">Dimensions</label>
@@ -37,9 +36,39 @@ function CustomGrid() {
                   id={`select-${idx}`}
                   className="w-full p-2 border border-gray-300 rounded outline-none bg-white"
                 >
-                  <option value="option1">Option1</option>
-                  <option value="option2">Option2</option>
+                  <option value="option1">Option 1</option>
+                  <option value="option2">Option 2</option>
                 </select>
+              </>
+            ) : idx === 3 ? (
+              <>
+                <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Length</label>
+                <input
+                  id={`input-${idx}`}
+                  type="number"
+                  placeholder="Enter length"
+                  className="w-full p-2 border border-gray-300 rounded"
+                />
+              </>
+            ) : idx === 4 ? (
+              <>
+                <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Width</label>
+                <input
+                  id={`input-${idx}`}
+                  type="number"
+                  placeholder="Enter width"
+                  className="w-full p-2 border border-gray-300 rounded"
+                />
+              </>
+            ) : idx === 5 ? (
+              <>
+                <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Height</label>
+                <input
+                  id={`input-${idx}`}
+                  type="number"
+                  placeholder="Enter height"
+                  className="w-full p-2 border border-gray-300 rounded"
+                />
               </>
             ) : (
               <>
@@ -52,7 +81,6 @@ function CustomGrid() {
                 />
               </>
             )}
-
           </div>
         ))}
       </div>
@@ -61,6 +89,7 @@ function CustomGrid() {
 }
 
 export default CustomGrid;
+
 
 
 
