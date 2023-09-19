1bb362e93f0028e2cc86c78604fcf82e364f8bce
Martin Mroz <mamr@hotmail.no>
2023-09-20 00:05:05 +0200

    volume calc

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function CustomGrid() {
+  const [length, setLength] = useState('');
+  const [width, setWidth] = useState('');
+  const [height, setHeight] = useState('');
+  const [volume, setVolume] = useState('');
+
+  useEffect(() => {
+    if (length && width && height) {
+      const lengthMeters = parseFloat(length) / 100;
+      const widthMeters = parseFloat(width) / 100;
+      const heightMeters = parseFloat(height) / 100;
+
+      const calculatedVolume = lengthMeters * widthMeters * heightMeters;
+      setVolume(calculatedVolume.toFixed(3));
+    }
+  }, [length, width, height]);
+
   return (
     <div className="mx-auto max-w-screen-xl px-5 py-10 flex flex-col items-center">
       <h1 className='text-center mb-5'>Volume weight</h1>
@@ -46,42 +62,48 @@ function CustomGrid() {
                   </select>
                 </div>
               );
-            case 3:
-              return (
-                <div key={idx} className="flex flex-col">
-                  <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Length</label>
-                  <input
-                    id={`input-${idx}`}
-                    type="number"
-                    placeholder="Enter length"
-                    className="w-full p-2 border border-gray-300 rounded"
-                  />
-                </div>
+              case 3:
+                return (
+                  <div key={idx} className="flex flex-col">
+                    <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Length</label>
+                    <input
+                      id={`input-${idx}`}
+                      type="number"
+                      placeholder="Enter length"
+                      className="w-full p-2 border border-gray-300 rounded"
+                      value={length}
+                      onChange={(e) => setLength(e.target.value)}
+                    />
+                  </div>
               );
-            case 4:
-              return (
-                <div key={idx} className="flex flex-col">
-                  <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Width</label>
-                  <input
-                    id={`input-${idx}`}
-                    type="number"
-                    placeholder="Enter width"
-                    className="w-full p-2 border border-gray-300 rounded"
-                  />
-                </div>
-              );
-            case 5:
-              return (
-                <div key={idx} className="flex flex-col">
-                  <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Height</label>
-                  <input
-                    id={`input-${idx}`}
-                    type="number"
-                    placeholder="Enter height"
-                    className="w-full p-2 border border-gray-300 rounded"
-                  />
-                </div>
-              );
+              case 4:
+                return (
+                  <div key={idx} className="flex flex-col">
+                    <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Width</label>
+                    <input
+                      id={`input-${idx}`}
+                      type="number"
+                      placeholder="Enter width"
+                      className="w-full p-2 border border-gray-300 rounded"
+                      value={width}
+                      onChange={(e) => setWidth(e.target.value)}
+                    />
+                  </div>
+                );
+                case 5:
+                  return (
+                    <div key={idx} className="flex flex-col">
+                      <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Height</label>
+                      <input
+                        id={`input-${idx}`}
+                        type="number"
+                        placeholder="Enter height"
+                        className="w-full p-2 border border-gray-300 rounded"
+                        value={height}
+                        onChange={(e) => setHeight(e.target.value)}
+                      />
+                    </div>
+                  );
               case 6:
                 return (
                   <div key={idx} className="flex flex-col">
@@ -106,19 +128,19 @@ function CustomGrid() {
                       />
                     </div>
                   );
-                case 8:
-                  return (
-                    <div key={idx} className="flex flex-col">
-                      <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Input {idx + 1}</label>
-                      <input
-                        id={`input-${idx}`}
-                        type="text"
-                        placeholder={`Input ${idx + 1}`}
-                        className="w-full p-2 border border-gray-300 rounded bg-gray-300"
-                        disabled
-                      />
-                    </div>
-                  );
+                  case 8:
+                    return (
+                      <div key={idx} className="flex flex-col">
+                        <label htmlFor={`input-${idx}`} className="mb-2 text-gray-600">Volume m3</label>
+                        <input
+                          id={`input-${idx}`}
+                          type="text"
+                          value={volume}
+                          className="w-full p-2 border border-gray-300 rounded bg-gray-300"
+                          disabled
+                        />
+                      </div>
+                    );
                 default:
                   return (
                     <div key={idx} className="flex flex-col">
@@ -133,6 +155,14 @@ function CustomGrid() {
                   );
               }
             })}
+
+            <hr className="col-span-3 my-5 border-t border-gray-300" />
+
+            
+        <button className="col-span-3 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+          Add row
+        </button>
+
       </div>
     </div>
   );
