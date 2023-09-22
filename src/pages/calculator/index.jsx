7f550d8ea184ae6734fb6d784755abe381e53de6
Martin Mroz <mamr@hotmail.no>
2023-09-22 16:47:08 +0200

    button size on lg-screens

--- src/pages/calculator/index.jsx
+++ src/pages/calculator/index.jsx
@@ -179,7 +179,7 @@ const handleQuantityChange = (e) => {
             <hr className="col-span-3 my-5 border-t border-gray-300" />
 
             
-        <button className="col-span-3 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+        <button className="col-span-3 lg:col-span-1 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
           Add row
         </button>
 
